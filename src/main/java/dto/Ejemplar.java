e61157086813510c3c11f6c9749e5044c6f5f94d
panik <panikillo173@gmail.com>
2024-12-10 09:58:28 +0100

    Initial commit

---
+++ src/main/java/dto/Ejemplar.java
@@ -0,0 +1,57 @@
+package dto;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "ejemplar")
+public class Ejemplar {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "isbn", nullable = false)
+    private dto.Libro isbn;
+
+    @ColumnDefault("'Disponible'")
+    @Lob
+    @Column(name = "estado")
+    private String estado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public dto.Libro getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(dto.Libro isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Ejemplar{" +
+                "id=" + id +
+                ", isbn=" + isbn +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+}
